ac4fdcfd2d80ebe0a31c88810bfd39b3c729df23
Кирилл Владимиров <kirilloriginallolo@gmail.com>
2024-03-15 02:08:41 +0700

    Add test components

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import {NavigationMenuDemo} from "@/components/ui/NavigationMenuDemo.tsx";
+
+
+function HomePage() {
+    return(
+        <NavigationMenuDemo/>
+    )
+}
+
+export default HomePage
